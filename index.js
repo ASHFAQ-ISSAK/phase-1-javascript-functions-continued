f235ae4cab2054e877458de064dde68ad2b09afc
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-03-14 13:07:18 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+
+}
+saturdayFun("bathe my dog")
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+console.log(mondayWork())
+
+function wrapAdjective(alphabet){
+    return function adjective(message){
+        return `You are ${alphabet}${message}${alphabet}!`
+        
+    }
+}
+console.log(wrapAdjective("*"))
